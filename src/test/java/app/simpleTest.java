e6f0552bbfe8cb35e7da1c8142cc087250e560e8
ost96 <37448385+ost96@users.noreply.github.com>
2018-11-24 03:46:55 +0100

    Creation of 2 tests 1 falid 1 unfalid

--- src/test/java/app/simpleTest.java
+++ src/test/java/app/simpleTest.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 public class simpleTest {
 
     @Test
-    public void simpleTest(){
+    public void simpleTest1(){
+        assertTrue(1 + 1 == 23);
+    }
+    @Test
+    public void simpleTest2(){
         assertTrue(1 + 1 == 2);
     }
 }
