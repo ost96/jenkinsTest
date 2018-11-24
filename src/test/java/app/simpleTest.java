625fb8ad12e13e01decebbc5bf670d8a3296c13d
ost96 <37448385+ost96@users.noreply.github.com>
2018-11-24 03:25:50 +0100

    Creation ov test that returns failure

---
+++ src/test/java/app/simpleTest.java
@@ -0,0 +1,13 @@
+package app;
+
+import static  org.junit.Assert.*;
+import org.junit.Test;
+
+public class simpleTest {
+
+    @Test
+    public void simpleTest(){
+        assertTrue(1 + 1 == 3);
+    }
+}
+
